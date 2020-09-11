79ae4d55b205b64b8177ffe19fa6be696aabb4e1
Alex Olivier <alex.olivier@valtech.com>
2020-09-11 10:38:31 -0700

    Adding tab menu + overlay
    - Added serum markers to scenario1
    - Added blurred overlays that track markers with a cutout
    - Added tab menu that is controlled by swiping

--- src/all-scenarios/in-store.js
+++ src/all-scenarios/in-store.js
@@ -16,6 +16,8 @@ ready(() => {
   const $tray = document.querySelector('#tray');
   const $backButton = document.querySelector('a.back');
   const $addButton = document.querySelector('a.add');
+  const $serumMarkers = document.querySelectorAll('a-marker');
+  const $tabMenu = document.querySelector('#tab-menu');
 
   /** Two useful functions:
     * - pause(X) 
@@ -132,9 +134,45 @@ ready(() => {
     return false;
   }
 
-  function configureScenario1() {
-    const $tabMenu = document.getElementById('tab-menu');
+  function tagToProduct(target) {
+    switch(target) {
+      case "serum_1_marker":
+        return 1;
+      case "serum_2_marker":
+        return 2;
+      case "serum_3_marker":
+        return 3;
+      default:
+      return null;
+    }
   }
+
+
+    $serumMarkers.forEach(function($marker) {
+
+      $marker.addEventListener("markerFound", (e)=>{ // your code here}
+        let target = e.target;
+        let productID = tagToProduct(target.id);
+
+        if (productID) {
+          console.log("Detected product ", productID);
+
+          // call productRecognized
+        }
+      })
+
+      $marker.addEventListener("markerLost", (e)=>{ // your code here}
+        let target = e.target;
+        let productID = tagToProduct(target.id);
+
+        if (productID) {
+          console.log("Lost product ", productID);
+
+          // Call productOutOfView
+        }
+      })
+    })
+
 
   // Add custom listeners for events when marker is recognized
   document.addEventListener('markerfound', productRecognized);
@@ -145,5 +183,68 @@ ready(() => {
   $addButton.addEventListener('click', addToRoutine);
   // This is temporary, until we have onmarkerfound event...
   $tray.addEventListener('click', productRecognized);
+
+  // Swipe detection
+  document.addEventListener('touchstart', handleTouchStart, false);
+  document.addEventListener('touchmove', handleTouchMove, false);
+
+  // document.addEventListener('swipeleft', handleSwipeLeft, false);
+  // document.addEventListener('swiperight', handleSwipeRight, false);
+
+  function handleSwipeLeft() {
+    console.log("swipe left!")
+    $tabMenu.components["tab-menu"].incrementSelectedIndex()
+    // $tabMenu.components.
+  }
+
+  function handleSwipeRight() {
+    console.log("swipe right!")
+    $tabMenu.components["tab-menu"].decrementSelectedIndex()
+  }
+
+  var xDown = null;
+  var yDown = null;
+
+  function getTouches(evt) {
+    return evt.touches ||             // browser API
+      evt.originalEvent.touches; // jQuery
+  }
+
+  function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];
+    xDown = firstTouch.clientX;
+    yDown = firstTouch.clientY;
+  };
+
+  function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+      return;
+    }
+
+    var xUp = evt.touches[0].clientX;
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+      if ( xDiff > 0 ) {
+        /* left swipe */
+        handleSwipeLeft()
+      } else {
+        handleSwipeRight()
+        /* right swipe */
+      }
+    } else {
+      if ( yDiff > 0 ) {
+        /* up swipe */
+      } else {
+        /* down swipe */
+      }
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;
+  };
 
 })
--- src/components/tab-menu.js
+++ src/components/tab-menu.js
@@ -11,38 +11,64 @@ AFRAME.registerComponent('tab-menu', {
   },
   init: function () {
 
-    this.tabElements = this.el.querySelectorAll('[tabitem]');
-
-    let children = this.el.querySelectorAll('[tabitem]');
+    this.tabElements = this.el.querySelectorAll('[tabitem], [icon-tab-menu-item]');
+    this.tabComponents = []
     this.spacing = this.data.tabSpacing
 
     this.selectedIndex = 0;
     let self = this;
 
+    console.log(this.tabElements)
+
+    // Fetch components
     this.tabElements.forEach( function(tag, index) {
-      let tabComponent = tag.components.tabitem
+      let textTab = tag.components['tabitem'];
+      let iconTab = tag.components['icon-tab-menu-item'];
+      let tabComponent = (textTab) ? textTab : iconTab;
+
+      self.tabComponents.push(tabComponent);
+    });
+
+    this.tabComponents.forEach( function(tabComponent, index) {
+
       let tabWidth = tabComponent.getWidth();
-      tag.object3D.position.x = tabWidth * 0.5 + index * self.data.tabSpacing
+      console.log(tabComponent)
+      tabComponent.el.object3D.position.x = tabWidth * 0.5 + index * self.data.tabSpacing
 
       if (index == self.selectedIndex) {
         tabComponent.select();
       } else {
         tabComponent.deselect();
       }
-
-      // tag.setAttribute("adjusted-rotation", "")
     })
   },
   selectIndex: function(index) {
+
     let self = this
-    this.tabElements.forEach( function(tabEl, i) {
-      let tabComponent = tabEl.components.tabitem
+    this.tabComponents.forEach( function(tabComponent, i) {
+      // let tabComponent = tabEl.components.tabitem
       if (i == index) {
-        tabComponent.select()
+        self.selectedIndex = index;
+        tabComponent.select();
+        console.log("selecting", index)
       } else {
-        tabComponent.deselect()
+        tabComponent.deselect();
       }
     })
+  },
+  incrementSelectedIndex: function() {
+    let numTabs = this.tabComponents.length
+    if (numTabs > 0) {
+      let indexToSelect = (this.selectedIndex + 1) % numTabs;
+      this.selectIndex(indexToSelect);
+    }
+  },
+  decrementSelectedIndex: function() {
+    let numTabs = this.tabComponents.length
+    if (numTabs > 0) {
+      let indexToSelect = (this.selectedIndex == 0) ? numTabs - 1 : (this.selectedIndex - 1) % numTabs;
+      this.selectIndex(indexToSelect);
+    }
   },
   update: function () {},
   tick: function () {
